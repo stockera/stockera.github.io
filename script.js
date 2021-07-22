1299bd9576a98ea5fa18a810fe69c935415b9052
stockera <58486254+stockera@users.noreply.github.com>
2021-07-22 18:05:03 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ $(function () {
         if ( $(window).scrollTop() > 10 ) {
             $('.navbar').removeClass('navbar-light');
             $('.navbar').addClass('navbar-dark');
+            $('.navbar').addClass('bg-dark');
         }
     });
 });
