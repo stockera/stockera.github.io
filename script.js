119d38d7a7e96d7ae88e164671e5a6ef36060f86
stockera <58486254+stockera@users.noreply.github.com>
2021-07-22 18:03:21 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 $(function () {
     $(window).on('scroll', function () {
         if ( $(window).scrollTop() > 10 ) {
-            $('.navbar').addClass('active');
-        } else {
-            $('.navbar').removeClass('active');
+            $('.navbar').removeClass('navbar-light');
+            $('.navbar').addClass('navbar-dark');
         }
     });
 });
