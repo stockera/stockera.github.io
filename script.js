88a52d6116042fe414cf4c9d2b83ee75f1352bb8
stockera <58486254+stockera@users.noreply.github.com>
2021-07-23 15:02:35 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,13 +3,15 @@ $( function () {
         var hauteurJumb = document.getElementById("jumb").offsetHeight;
         var hauteurNav = document.getElementById("Nav").offsetHeight;
         if ( $(window).scrollTop() > hauteurJumb-hauteurNav ) {
-            $('.navbar').removeClass('navbar-light');
+           /* $('.navbar').removeClass('navbar-light');
             $('.navbar').addClass('navbar-dark');
-            $('.navbar').addClass('bg-dark');
+            $('.navbar').addClass('bg-dark');*/
+            $('.navbar').addClass('bg-scroll');
         } else {
-            $('.navbar').removeClass('navbar-dark');
+            /*$('.navbar').removeClass('navbar-dark');
             $('.navbar').removeClass('bg-dark');
-            $('.navbar').addClass('navbar-light');
+            $('.navbar').addClass('navbar-light');*/
+            $('.navbar').removeClass('bg-scroll');
         }
     });
 });
